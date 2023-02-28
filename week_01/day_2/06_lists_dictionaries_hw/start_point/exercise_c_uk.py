ac9a35a7cf5295182167ae7db6b163f2f54e967a
Sky Su <skysu.code@gmail.com>
2023-02-28 16:26:23 +0000

    fix indentation

--- week_01/day_2/06_lists_dictionaries_hw/start_point/exercise_c_uk.py
+++ week_01/day_2/06_lists_dictionaries_hw/start_point/exercise_c_uk.py
@@ -1,19 +1,19 @@
 united_kingdom = [
-  {
-    "name": "Scotland",
-    "population": 5295000,
-    "capital": "Edinburgh"
-  },
-  {
-    "name": "Wales",
-    "population": 3063000,
-    "capital": "Swansea"
-  },
-  {
-    "name": "England",
-    "population": 53010000,
-    "capital": "London"
-  }
+    {
+        "name": "Scotland",
+        "population": 5295000,
+        "capital": "Edinburgh"
+    },
+    {
+        "name": "Wales",
+        "population": 3063000,
+        "capital": "Swansea"
+    },
+    {
+        "name": "England",
+        "population": 53010000,
+        "capital": "London"
+    }
 ]
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
